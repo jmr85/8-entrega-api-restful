20a3b37b4b9d29a2d01716a5e8ed798effdb659d
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-08-02 04:14:50 -0300

    added save app.post('/api/productos')

--- index.js
+++ index.js
@@ -37,6 +37,31 @@ app.get('/api/productos/:id', async (req, res) => {
     }
 });
 
+app.post('/api/productos', async(req, res) => {
+    const {title, price, thumbnail} = req.body;
+   
+    const validator = (title !== null && price !==null && thumbnail !== null) 
+    && (title !== '' && price !== '' && thumbnail !== '');
+    if (validator) {
+        try {
+            await contenedor.save(title, price, thumbnail);//almaceno en el file el producto
+            
+            const allPruductos = await contenedor.getAll();          
+            const productAdded = allPruductos.length;//Para sacar el ultimo index
+            const producto = await contenedor.getById(productAdded);//Obtengo el ultimo producto agregado
+           
+            return res.status(201).json(producto);//Muestra el ultimo producto agregado
+        }catch (err) {
+            return res.status(500).json({
+                message: 'Error al guardar el producto',
+                err
+            });
+        }
+    }else{
+        return res.status(400).json({message: 'Campos invalidos o vacios'});
+    }
+});
+
 app.get('/api/productoRandom', async (req, res) => {
     let productos;
     try {
